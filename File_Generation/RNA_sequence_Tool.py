16433be8f36cf4d30ed21aa8fd9e60d546b9b65e
pronay <pronoypaul.500@gmail.com>
2025-12-30 23:34:24 +0530

    Code added with new source file.

--- File_Generation/RNA_sequence_Tool.py
+++ File_Generation/RNA_sequence_Tool.py
@@ -5,13 +5,14 @@ import compare_genes_amino as cga
 import format_gene_result
 
 if __name__ == "__main__":
-    if len(argv) != 4:
-        print("Usage: python script_name.py <source_file_path1> <source_file_path2> <destination_file_path>")
+    if len(argv) != 5:
+        print("Usage: python script_name.py <source_file_path1> <source_file_path2> <source_file_path3> <destination_file_path>")
     source_path_gene = os.path.abspath(argv[1])
     source_path_amino = os.path.abspath(argv[2])
-    destination_path = os.path.abspath(argv[3])
+    source_path_reference = os.path.abspath(argv[3])
+    destination_path = os.path.abspath(argv[4])
 
-    final_dataset = cga.compare(source_path_gene, source_path_amino)
+    final_dataset = cga.compare(source_path_gene, source_path_amino, source_path_reference)
 
     # Writing to excel
     final_dataset.index = range(1, len(final_dataset) + 1)
--- File_Generation/compare_genes_amino.py
+++ File_Generation/compare_genes_amino.py
@@ -1,11 +1,13 @@
 """
 Compares the data from the Amino_acid_biosynthesis_with_bold_headings xlsx file and gene_result.txt
+Reference it with the final sample for complete result
 """
+import pandas as pd
 import format_gene_result as gr
 import format_amino_reference as ar
 
 
-def compare(source1, source2):
+def compare(source1, source2, source3):
     gene_dataframe = gr.format_gene_result(source1)
     amino_dataframe = ar.create_amino_acid_dataframe(source2)
 
@@ -16,8 +18,51 @@ def compare(source1, source2):
         how='left'
     )
 
-    return combined
+    final_reference_dataframe = pd.read_excel(source3, header=1)
+    formated_reference_dataframe = final_reference_dataframe.loc[:,['ID', 'log2FoldChange', 'pvalue', 'padj']]
+    # print(formated_reference_dataframe.to_string())
+
+    # Adding a temporary order column
+    combined['_order'] = range(len(combined))
+
+    # Merge on Alias1
+    m1 = combined.merge(
+        formated_reference_dataframe,
+        left_on='Alias1',
+        right_on='ID',
+        how='left'
+    )
+
+    # Merge on Alias2
+    m2 = combined.merge(
+        formated_reference_dataframe,
+        left_on='Alias2',
+        right_on='ID',
+        how='left'
+    )
+
+    # Combine columns (Alias1 → Alias2 fallback)
+    final_combined = combined.copy()
+
+    for col in ['log2FoldChange', 'pvalue', 'padj']:
+        final_combined[col] = (
+            m1[col].combine_first(m2[col])
+        )
+
+    # Restore original order & cleanup
+    final_combined = (
+        final_combined.sort_values('_order').drop(columns=['_order']).reset_index(drop=True)
+    )
+
+    final_combined = final_combined.dropna(subset=['log2FoldChange', 'pvalue', 'padj'],how='all')
+
+    return final_combined
 
 
-# combined_dataset = compare('Input_Files/gene_result.txt')
-# print(combined_dataset.to_string())
+# combined_dataset = (
+# final_combined_df = compare('Input_Files/gene_result.txt',
+#                        'Input_Files/List_of_Vibrio_genes.xlsx',
+#                        'Input_Files/Sample_Gene_Reference.xlsx')
+#
+# final_combined_df.to_excel('Output_Files/test.xlsx', na_rep='N/A', index=True, index_label='Sl_No')
+# print("Excel file write completed.....")
